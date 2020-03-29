ed326d1c7d4c88c703a6380ad1673e179614ee20
jessi ortega <ortegabx05@gmail.com>
2020-03-29 01:34:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ require_relative "../config/environment.rb"
 class Student
  attr_accessor :id, :name, :grade
 
-def initialize(id=nil, name, grade)
+  def initialize(id=nil, name, grade)
   @id, @name, @grade = id, name, grade
+end
 
 end
