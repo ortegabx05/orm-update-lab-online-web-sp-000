4e0490af9154f8149786ff602856b285c607517c
jessi ortega <ortegabx05@gmail.com>
2020-03-29 01:45:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,9 +37,9 @@ class Student
   end
   
    def self.create(name, grade)
-    student_new = self.new(name, grade)
-    student_new.save
-    student_new
+    student = self.new(name, grade)
+    student.save
+    student
   end
   
   def self.new_from_db(row)
