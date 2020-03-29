3001672a53852555544f55cc1f486fbe6224090d
jessi ortega <ortegabx05@gmail.com>
2020-03-29 01:35:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,5 +6,12 @@ class Student
   def initialize(id=nil, name, grade)
     @id, @name, @grade = id, name, grade
   end
-
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT, 
+        grade TEXT
+  end
 end
